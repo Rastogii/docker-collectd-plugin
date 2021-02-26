5cdff293ef99623b98fc438ecea21db27a829ae9
Rastogii <vallarirastogi03@gmail.com>
2021-02-26 08:16:41 +0530

    Update dockerplugin.py

--- dockerplugin.py
+++ dockerplugin.py
@@ -646,12 +646,12 @@ class DockerPlugin:
 
         try:
             version = self.client.version()['ApiVersion']
-        except IOError as e:
+        except IOError as err:
             collectd.warning((
                     'Unable to access Docker daemon at {url} in \
                     init_callback. Will try in read_callback.'
                     'This may indicate SELinux problems. : {error}')
-                    .format(url=self.docker_url, error=e))
+                    .format(url=self.docker_url, error=err))
 
             collectd.register_read(self.read_callback, interval=COLLECTION_INTERVAL)
 
